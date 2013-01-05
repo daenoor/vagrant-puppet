5b83449235f367123388a172df3684dc6f509fcd
Andrey Yaroshenko <da3n00r@gmail.com>
2013-01-06 06:53:02 +0700

    Set vhost via apache::site
    Now you can pass 'content' option with vhost file content

--- vagrant/modules/apache/manifests/site.pp
+++ vagrant/modules/apache/manifests/site.pp
@@ -1,28 +1,40 @@
-define apache::site ($ensure='present') {
-	$a2ensite_deps = Package["apache"]
+define apache::site (
+    $ensure='present',
+    $content = ''
+) {
+    $a2ensite_deps = Package["apache"]
 
-	case $ensure {
-		"present": {
-			exec { "a2ensite ${name}":
-				command => "/usr/sbin/a2ensite ${name}",
-				require => $a2ensite_deps,
-				notify => Service["apache"],
-				unless => "/bin/sh -c '[ -L /etc/apache2/sites-enabled/${name} ] \\
-					&& [ /etc/apache2/sites-enabled/${name} -ef /etc/apache2/sites-available/${name} ]'",
-			}
-		}
+    case $ensure {
+        "present": {
+            if $content {
+                file { "/etc/apache2/sites-available/${name}":
+                    ensure => file,
+                    content => $content,
+                    before => Exec["a2ensite ${name}"],
+                    require => $a2ensite_deps,
+                }
+            }
 
-		"absent": {
-			exec { "a2dissite ${name}":
-				command => "/usr/sbin/a2dissite ${name}",
-				require => $a2ensite_deps,
-				notify => Service["apache"],
-				onlyif => "/bin/sh -c '[ -L /etc/apache2/sites-enabled/${name} ] && [ -e /etc/apache2/sites-enabled/${name} ]'",
-			}
-		}
+            exec { "a2ensite ${name}":
+                command => "/usr/sbin/a2ensite ${name}",
+                require => $a2ensite_deps,
+                notify => Service["apache"],
+                unless => "/bin/sh -c '[ -L /etc/apache2/sites-enabled/${name} ] \\
+                    && [ /etc/apache2/sites-enabled/${name} -ef /etc/apache2/sites-available/${name} ]'",
+            }
+        }
 
-		default: {
-			err ( "Unknown ensure value: '${ensure}'" )
-		}
-	}
+        "absent": {
+            exec { "a2dissite ${name}":
+                command => "/usr/sbin/a2dissite ${name}",
+                require => $a2ensite_deps,
+                notify => Service["apache"],
+                onlyif => "/bin/sh -c '[ -L /etc/apache2/sites-enabled/${name} ] && [ -e /etc/apache2/sites-enabled/${name} ]'",
+            }
+        }
+
+        default: {
+            err ( "Unknown ensure value: '${ensure}'" )
+        }
+    }
 }
