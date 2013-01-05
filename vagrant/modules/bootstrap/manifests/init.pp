613726958ccf9e5ea926600e05a13d8509d8611b
Andrey Yaroshenko <da3n00r@gmail.com>
2013-01-06 01:26:07 +0700

    set app qualification path, fix indentation

--- vagrant/modules/bootstrap/manifests/init.pp
+++ vagrant/modules/bootstrap/manifests/init.pp
@@ -2,32 +2,35 @@
 #
 # Preparing environment
 class bootstrap {
-	group { 'puppet':
-    	ensure => 'present'
-  	}
 
-  	file { "10gen repository":
-  		ensure => file,
-  		path => "/etc/apt/sources.list.d/10gen.list",
-  		owner => "root",
-  		group => "root",
-  		mode => "0644",
-  		content => "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen",
-  	}
+    Exec { path => '/usr/bin:/bin:/usr/sbin:/sbin' }
 
-  	exec { "Import 10gen key":
-  		command => "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10",
-  	}
+    group { 'puppet':
+        ensure => 'present'
+    }
 
-	# make packages list up to date
-  	exec { 'apt-get update':
-    	command => '/usr/bin/apt-get update',
-    	require => [File["10gen repository"], Exec["Import 10gen key"]],
-  	}
+    file { "10gen repository":
+        ensure => file,
+        path => "/etc/apt/sources.list.d/10gen.list",
+        owner => "root",
+        group => "root",
+        mode => "0644",
+        content => "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen",
+    }
 
-  	# install software
-  	package { ["build-essential", "git-core", "curl", "mercurial"]:
-  		ensure => latest,
-  		require => Exec["apt-get update"],
-  	}
+    exec { "Import 10gen key":
+        command => "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10",
+    }
+
+    # make packages list up to date
+    exec { 'apt-get update':
+        command => '/usr/bin/apt-get update',
+        require => [File["10gen repository"], Exec["Import 10gen key"]],
+    }
+
+    # install software
+    package { ["build-essential", "git-core", "curl", "mercurial"]:
+        ensure => latest,
+        require => Exec["apt-get update"],
+    }
 }
