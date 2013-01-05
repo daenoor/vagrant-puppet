b79f9f2268fec58e5262761635d4acd57534c553
Andrey Yaroshenko <da3n00r@gmail.com>
2013-01-06 05:50:57 +0700

    Removed php5-mysql installation, indentation fixes

--- vagrant/modules/php/manifests/init.pp
+++ vagrant/modules/php/manifests/init.pp
@@ -2,47 +2,39 @@
 #
 #
 class php {
-	$phpini = "/etc/php5/cli/php.ini"
-  	$phpinidir = "/etc/php5/conf.d/"
+    $phpini = "/etc/php5/cli/php.ini"
+    $phpinidir = "/etc/php5/conf.d/"
 
-  	package { "php5-cli":
-  		ensure => latest,
-  		require => Exec["apt-get update"],
-  	}
+    package { "php5-cli":
+        ensure => latest,
+        require => Exec["apt-get update"],
+    }
 
-  	package { "php-pear":
-  		ensure => latest,
-  		require => Package["php5-cli"],
-  	}
+    package { "php-pear":
+        ensure => latest,
+        require => Package["php5-cli"],
+    }
 
-  	php::module { "xdebug": 
-  		ensure => "present",
-  		before => File["xdebug config"],
-  	}
+    php::module { "xdebug": 
+        ensure => "present",
+        before => File["xdebug config"],
+    }
 
 
-  	if defined(Package['apache']) {
-		file { "xdebug config":
-			ensure => file,
-			path => "${phpinidir}xdebug.ini",
-			source => 'puppet:///modules/php/xdebug.ini',
-			require => Package['php5-cli'],
-			notify => Service['apache']
-		}
-  	}
-  	else {
-		file { "xdebug config":
-			ensure => file,
-			path => "${phpinidir}xdebug.ini",
-			source => 'puppet:///modules/php/xdebug.ini',
-			require => Package['php5-cli'],
-		}
-  	}
-
-    # If mysql installed install mysql extension
-    if defined(Service["mysql"]) {
-        php::module{ "mysql":
-            ensure => "present",
+    if defined(Package['apache']) {
+        file { "xdebug config":
+            ensure => file,
+            path => "${phpinidir}xdebug.ini",
+            source => 'puppet:///modules/php/xdebug.ini',
+            require => Package['php5-cli'],
+            notify => Service['apache']
+        }
+    }
+    else {
+        file { "xdebug config":
+            ensure => file,
+            path => "${phpinidir}xdebug.ini",
+            source => 'puppet:///modules/php/xdebug.ini',
             require => Package['php5-cli'],
         }
     }
