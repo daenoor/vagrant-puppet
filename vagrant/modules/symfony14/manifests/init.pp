7cb25c2fb8ec5c2226efe9efffd85555df7dced4
Andrey Yaroshenko <da3n00r@gmail.com>
2013-01-06 06:53:51 +0700

    create vhost via apache::site instead of creating it manually

--- vagrant/modules/symfony14/manifests/init.pp
+++ vagrant/modules/symfony14/manifests/init.pp
@@ -2,24 +2,18 @@
 #
 #
 class symfony14(
-	$targetdir = '/vagrant'
+    $targetdir = '/vagrant'
 ) {
-	php::pear::package{ "symfony":
-		repository => "pear.symfony-project.com",
-	}
+    php::pear::package{ "symfony":
+        repository => "pear.symfony-project.com",
+    }
 
-	file { "symfony vhost":
-		ensure => file,
-		path => "/etc/apache2/sites-available/symfony.lo",
-		content => template('symfony14/symfony.lo.erb')
-	}
+    apache::site{ "default":
+        ensure => absent,
+    }
 
-	apache::site{ "default":
-		ensure => absent,
-	}
-
-	apache::site{ "symfony.lo":
-		ensure => present,
-		require => File["symfony vhost"],
-	}
+    apache::site{ "symfony.lo":
+        ensure => present,
+        content => template('symfony14/symfony.lo.erb'),
+    }
 }
