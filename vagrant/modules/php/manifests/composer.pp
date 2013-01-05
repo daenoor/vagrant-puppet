6ae320ce01f493c7f6bede12179cb470dabc9068
Andrey Yaroshenko <da3n00r@gmail.com>
2013-01-06 05:51:37 +0700

    composer class fixes

--- vagrant/modules/php/manifests/composer.pp
+++ vagrant/modules/php/manifests/composer.pp
@@ -3,7 +3,7 @@
 #
 class php::composer {
 	exec { "composer":
-		command => "curl -s https://getcomposer.org/installer | php -- --install-dir=bin",
+		command => "curl -s http://getcomposer.org/installer | php -- --install-dir=/usr/local/bin",
 		require => [Package["curl"], Package["php5-cli"]],
 		user => root
 	}
