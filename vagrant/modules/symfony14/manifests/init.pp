14883543d7681bbcfef02f0257f99f225631ec95
Andrey Yaroshenko <da3n00r@gmail.com>
2013-01-06 06:15:21 +0700

    symfony14 vhost config

--- vagrant/modules/symfony14/manifests/init.pp
+++ vagrant/modules/symfony14/manifests/init.pp
@@ -1,8 +1,25 @@
 # Class: symfony14
 #
 #
-class symfony14 {
+class symfony14(
+	$targetdir = '/vagrant'
+) {
 	php::pear::package{ "symfony":
 		repository => "pear.symfony-project.com",
+	}
+
+	file { "symfony vhost":
+		ensure => file,
+		path => "/etc/apache2/sites-available/symfony.lo",
+		content => template('symfony14/symfony.lo.erb')
+	}
+
+	apache::site{ "default":
+		ensure => absent,
+	}
+
+	apache::site{ "symfony.lo":
+		ensure => present,
+		require => File["symfony vhost"],
 	}
 }
