54ba30913c0000b5e670ef5eba036ad41ee3f761
Andrey Yaroshenko <da3n00r@gmail.com>
2013-01-06 02:09:08 +0700

    require php5-cli before installing apache module

--- vagrant/modules/php/manifests/apache.pp
+++ vagrant/modules/php/manifests/apache.pp
@@ -3,6 +3,7 @@ class php::apache inherits php{
 	package { "apache php module":
 		name => "libapache2-mod-php5",
 		ensure => present,
+		require => Package['php5-cli'],
 	}
 
 	file { "${phpinidir}01-settings.ini":
