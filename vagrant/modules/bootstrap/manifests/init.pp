2bd5a149a0e98404123b6081d2bc5ab3bf8b3499
Andrey Yaroshenko <da3n00r@gmail.com>
2013-01-08 18:24:24 +0700

    bootstrap module fixes
    apt-get now is dependecy for every package installation, added bribhtbox
    ppa for ruby, added python-software-properties for add-apt-repostory
    command

--- vagrant/modules/bootstrap/manifests/init.pp
+++ vagrant/modules/bootstrap/manifests/init.pp
@@ -5,6 +5,14 @@ class bootstrap {
 
     Exec { path => '/usr/bin:/bin:/usr/sbin:/sbin' }
 
+    # make packages list up to date
+    exec { 'apt-get update':
+        command => '/usr/bin/apt-get update',
+        # require => [File["10gen repository"], Exec["Import 10gen key"]],
+    }
+
+    Exec["apt-get update"] -> Package <| |> 
+    
     group { 'puppet':
         ensure => 'present'
     }
@@ -22,15 +30,25 @@ class bootstrap {
         command => "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10",
     }
 
-    # make packages list up to date
-    exec { 'apt-get update':
-        command => '/usr/bin/apt-get update',
-        require => [File["10gen repository"], Exec["Import 10gen key"]],
-    }
-
     # install software
-    package { ["build-essential", "git-core", "curl", "mercurial"]:
+    package { ["build-essential", "git-core", "curl", "mercurial", "python-software-properties"]:
         ensure => latest,
         require => Exec["apt-get update"],
+    }
+
+    # Install ruby from Brightbox ppa
+    exec { "add brightbox ppa":
+        command => "apt-add-repository ppa:brightbox/ruby-ng",
+        require => Package["python-software-properties"],
+    }
+
+    exec { "use brightbox ppa":
+        command => "apt-get update",
+        require => Exec["add brightbox ppa"],
+    }
+
+    package { "ruby1.9.1":
+        ensure => latest,
+        require => Exec["use brightbox ppa"],
     }
 }
