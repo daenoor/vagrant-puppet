87159539f92cee73b730bb98c5dda96781d8094b
Andrey Yaroshenko <da3n00r@gmail.com>
2013-04-20 03:43:29 +0700

    working on new manifests

---
+++ vagrant/manifests/users.pp
@@ -0,0 +1,44 @@
+# Class: base::users
+# 
+# Sample users and groups
+#
+class base::users {
+    
+    # Groups
+    @group { "admins":
+        gid => '999',
+        tag => "admins",
+        ensure => present,
+    }
+
+    @group { "developers":
+        gid => '998',
+        tag => "developers",
+        ensure => present,
+    }
+
+    @group { "deployers":
+        gid => '997',
+        tag => "deployers",
+        ensure => present,
+    }
+
+    # Root
+    # password is 'puppet' (without quotes)
+    @user { "root":
+        ensure => present,
+        password => '$6$$1z48.X3KuWbjAGh5yZ0g2cZkcPiEDZYSwzJQ0SUtdgHHgVd0EVhC1efDIr6uu.nuQSKZY12P9lVPE0bM2znUV1',
+    }
+
+    # Users
+
+    @user::managed{ "developer":
+        ensure => present,
+        name_comment => "Developer",
+        uid => "1998",
+        managehome => true,
+        groups => "developers",
+        tag => "developers",
+        manage_groups => true,
+    }
+}
