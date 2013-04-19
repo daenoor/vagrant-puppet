9764649ceaf43235c3c388851f2cf0fa08eac902
Andrey Yaroshenko <da3n00r@gmail.com>
2013-04-20 02:48:12 +0700

    cleanup to start from scratch

--- vagrant/modules/capifony/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-# Class: capifony
-#
-#
-class capifony {
-    exec { "install capifony":
-        command => "gem install capifony --no-ri --no-rdoc",
-        require => Package["ruby1.9.1"],
-    }
-}
--- vagrant/modules/laravel/files/application_local.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-return array(
-    'profiler' => true, 
-);
