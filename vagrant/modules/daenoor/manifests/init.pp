10dff38965e326e7013f2093f073d2d83adc3b04
Andrey Yaroshenko <da3n00r@gmail.com>
2013-04-24 06:04:07 +0700

    fixed feature import

--- vagrant/modules/daenoor/manifests/init.pp
+++ vagrant/modules/daenoor/manifests/init.pp
@@ -6,7 +6,7 @@ class daenoor
     include daenoor::base::system
 
     define enable_features {
-        include "daenoor::features::feature_${name}"
+        include "daenoor::feature::feature_${name}"
     }
 
     enable_features($::features)
