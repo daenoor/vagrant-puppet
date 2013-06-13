4e5382e525ebdb6ebc1fd3ca3d53daf2eb627283
Andrey Yaroshenko <da3n00r@gmail.com>
2013-06-14 01:32:24 +0700

    added ruby requirement for foundation gems

--- vagrant/modules/daenoor/manifests/feature/feature_foundation.pp
+++ vagrant/modules/daenoor/manifests/feature/feature_foundation.pp
@@ -4,9 +4,11 @@ class daenoor::feature::feature_foundation {
     include daenoor::ruby
 
     rvm_gem { 
-        'compass': 
-            ensure => latest;
-        'zurb-foundation': 
-            ensure => latest;
+        'ruby-1.9.3/compass': 
+            ensure => latest,
+            require => Rvm_system_ruby['ruby-1.9.3'];
+        'ruby-1.9.3/zurb-foundation': 
+            ensure => latest,
+            require => Rvm_system_ruby['ruby-1.9.3'];
     }
 }
