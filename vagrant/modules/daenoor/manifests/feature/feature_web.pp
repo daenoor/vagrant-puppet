b53adf152a68a87c3bb63e7bac4b7ec18991c41a
Andrey Yaroshenko <da3n00r@gmail.com>
2013-06-14 12:20:13 +0700

    removed duplicated module enabbling, renamed vhost generation task name

--- vagrant/modules/daenoor/manifests/feature/feature_web.pp
+++ vagrant/modules/daenoor/manifests/feature/feature_web.pp
@@ -5,11 +5,8 @@ class daenoor::feature::feature_web {
 	# Apache
     class { 'apache':
     }
-  
-    apache::module { 'headers': }
-    apache::module { 'rewrite': }
 
-	apache::vhost { "${fqdn}":
+	apache::vhost { "app_vhost":
         name => "myapp",
         docroot => "/vagrant/web",
         template => 'daenoor/apache/vhost/symfony14/app.local.conf',
