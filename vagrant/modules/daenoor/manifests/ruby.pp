72c1d1f03452e5000a16d9f0383418084b25a82d
Andrey Yaroshenko <da3n00r@gmail.com>
2013-06-25 02:14:39 +0700

    don't set default ruby

--- vagrant/modules/daenoor/manifests/ruby.pp
+++ vagrant/modules/daenoor/manifests/ruby.pp
@@ -8,15 +8,8 @@ class daenoor::ruby {
 
     rvm_system_ruby {
         'ruby-1.9.3':
-            ensure => 'present',
-            default_use => true;
-        'ruby-1.8.7':
-        	ensure => 'present';
+            ensure => 'present';
     }
 
-    rvm_gem { 
-        'ruby-1.8.7/puppet': 
-            ensure => latest,
-            require => Rvm_system_ruby['ruby-1.8.7'];
-    }
+    
 }
